c6415204e1916d310f9cd420e5695c5dca5c3ba4
hcftrize <hcftrize@gmail.com>
2026-04-28 07:08:35 +0200

    Update mapping.ts

--- goldsky-governance/src/mapping.ts
+++ goldsky-governance/src/mapping.ts
@@ -1,7 +1,7 @@
 import { BigDecimal, BigInt } from "@graphprotocol/graph-ts";
 import { ethereum } from "@graphprotocol/graph-ts";
-import { GovernanceBonding } from "../generated/GovernanceBonding/GovernanceBonding";
-import { BondTimeMarkerSnapshot } from "../generated/schema";
+import { BondCreated } from "../generated/GovernanceBonding/GovernanceBonding";
+import { Bond, BondCreatedEvent, IncreaseBondEvent } from "../generated/schema";
 
 let DECIMALS = BigDecimal.fromString("1000000000000000000");
 
@@ -28,27 +28,58 @@ function tsToDateStr(ts: i64): string {
   return y.toString() + "-" + mm + "-" + dd;
 }
 
+export function handleBondCreated(event: BondCreated): void {
+  let nftId  = event.params.bondId;
+  let amount = event.params.amount.toBigDecimal().div(DECIMALS);
+  let date   = tsToDateStr(event.block.timestamp.toI64());
+
+  let ev        = new BondCreatedEvent(event.transaction.hash.toHex() + "-" + event.logIndex.toString());
+  ev.nftId      = nftId;
+  ev.owner      = event.params.owner;
+  ev.poolId     = event.params.poolId as i32;
+  ev.amount     = amount;
+  ev.date       = date;
+  ev.blockNumber = event.block.number;
+  ev.timestamp  = event.block.timestamp;
+  ev.txHash     = event.transaction.hash;
+  ev.save();
+
+  let bond = Bond.load(nftId.toString());
+  if (bond == null) {
+    bond = new Bond(nftId.toString());
+    bond.nftId               = nftId;
+    bond.owner               = event.params.owner;
+    bond.poolId              = event.params.poolId as i32;
+    bond.createdAtDate       = date;
+    bond.createdAtTimestamp  = event.block.timestamp;
+    bond.createdAtBlock      = event.block.number;
+    bond.totalDeposited      = amount;
+    bond.increaseCount       = 0;
+    bond.lastDepositDate     = date;
+    bond.lastDepositTimestamp = event.block.timestamp;
+  }
+  bond.save();
+}
+
 export function handleIncreaseBond(call: ethereum.Call): void {
   let nftId       = call.inputValues[0].value.toBigInt();
   let amountAdded = call.inputValues[1].value.toBigInt().toBigDecimal().div(DECIMALS);
+  let date        = tsToDateStr(call.block.timestamp.toI64());
 
-  let contract   = GovernanceBonding.bind(call.to);
-  let bondResult = contract.try_getBond(nftId);
-  if (bondResult.reverted) return;
-
-  let timeMarker = bondResult.value.getTimeMarker();
-  let amount     = bondResult.value.getAmount().toBigDecimal().div(DECIMALS);
-  let poolId     = bondResult.value.getPoolId() as i32;
-
-  let ev         = new BondTimeMarkerSnapshot(call.transaction.hash.toHex() + "-" + call.block.number.toString());
-  ev.nftId       = nftId;
-  ev.timeMarker  = timeMarker;
-  ev.amount      = amount;
-  ev.poolId      = poolId;
-  ev.amountAdded = amountAdded;
-  ev.date        = tsToDateStr(call.block.timestamp.toI64());
+  let ev        = new IncreaseBondEvent(call.transaction.hash.toHex() + "-" + call.block.number.toString());
+  ev.nftId      = nftId;
+  ev.amount     = amountAdded;
+  ev.date       = date;
   ev.blockNumber = call.block.number;
-  ev.timestamp   = call.block.timestamp;
-  ev.txHash      = call.transaction.hash;
+  ev.timestamp  = call.block.timestamp;
+  ev.txHash     = call.transaction.hash;
   ev.save();
+
+  let bond = Bond.load(nftId.toString());
+  if (bond == null) return;
+  bond.totalDeposited        = bond.totalDeposited.plus(amountAdded);
+  bond.increaseCount         = bond.increaseCount + 1;
+  bond.lastDepositDate       = date;
+  bond.lastDepositTimestamp  = call.block.timestamp;
+  bond.save();
 }
